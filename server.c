f3db26b1aa66c3af84be06b20fa0710304e1e6ef
Rian Wardana <gmail@rianwardana.com>
2017-12-07 21:45:48 +0700

    Diberi comment penjelasan program

--- server.c
+++ server.c
@@ -1,156 +1,194 @@
-#include<stdio.h>
-#include<string.h>    //strlen
-#include<stdlib.h>    //strlen
-#include<sys/socket.h>
-#include<arpa/inet.h> //inet_addr
-#include<unistd.h>    //write
-#include<pthread.h> //for threading , link with lpthread
+// Program ini dijalankan di server, yang mana
+// dalam hal ini kita menggunakan Ubuntu Server 16.04 LTS
+// di alamat 103.43.44.105 (http://chatmote.com)
 
-//// Include MySQL ////
+// Melampirkan library-library yang dibutuhkan untuk
+// socket programming dan multi-threading.
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+#include<pthread.h>     // (link with -lpthread)
+
+// Melampirkan library yang dibutuhkan untuk
+// terhubung dengan database MySQL yang terinstall
+// pada server. Hal ini karena pesan-pesan yang
+// ditulis oleh pengguna disimpan di database ini.
 #include <my_global.h>
 #include <mysql.h>
  
-//the thread function
+// Digunakan untuk multi-threading (pthread)
 void *connection_handler(void *);
 
-//punya mysql
+// Digunakan apabila pada proses yang berhubungan
+// dengan MySQL terdapat eror.
 void finish_with_error(MYSQL *con);
- 
-int main(int argc , char *argv[])
-{
+
+// Program utama yang fungsinya adalah untuk
+// menerima pesan dari pengguna, dan menyimpannya
+// di database MySQL.
+int main(int argc , char *argv[]) {
+    // Adalah variabel dalam bagian socket programming
     int socket_desc , client_sock , c , *new_sock;
     struct sockaddr_in server , client;
      
-    //Create socket
+    // Membuat soket baru, dan memberikan kabar
+    // perihal apakah hal tersebut sukses dilakukan
+    // atau tidak.
     socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-    if (socket_desc == -1)
-    {
+    if (socket_desc == -1) {
         printf("Could not create socket");
-    }
-    puts("Socket created");
-     
-    //Prepare the sockaddr_in structure
+    } puts("Socket created");
+    
+    // Untuk socket programming
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons( 8888 );
+
+    // Pada port 8888 lah program server ini akan
+    // mendengarkan (listen) request dari client
+    // (komputer pengguna). Atau dalam kata lain,
+    // apabila ada request yang masuk ke server ini
+    // pada port 8888, maka program inilah yang akan
+    // meresponnya.
+    server.sin_port = htons(8888);
      
-    //Bind
-    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
-    {
-        //print the error message
+    // Mulai mendengarkan (listen) request yang masuk ke program ini,
+    // dan mengabarkan apakah hal tersebut sukses dilakukan atau tidak.
+    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0) {
         perror("bind failed. Error");
         return 1;
-    }
-    puts("bind done");
+    } puts("bind done");
+    listen(socket_desc, 3);
      
-    //Listen
-    listen(socket_desc , 3);
-     
-    //Accept and incoming connection
+    // Mengabarkan bahwa program ini sudah siap dan sedang 
+    // menunggu request dari komputer pengguna
     puts("Waiting for incoming connections...");
     c = sizeof(struct sockaddr_in);
 
-	
-    /* 
-    //Accept and incoming connection
-    puts("Waiting for incoming connections...");
-    c = sizeof(struct sockaddr_in); */
-    while( (client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) )
-    {
+    // Bagian ini akan dijalankan jika program menerima request dari
+    // komputer pengguna.
+    while( (client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) ) {
         puts("Connection accepted");
-         
+
+        // Untuk pthread
         pthread_t sniffer_thread;
+
+        // Untuk socket programming
         new_sock = malloc(1);
         *new_sock = client_sock;
          
-        if( pthread_create( &sniffer_thread , NULL ,  connection_handler , (void*) new_sock) < 0)
-        {
+        // Implementasi pthread sehingga request yang diterima selanjutnya akan
+        // akan diproses pada thread lain (thread baru).
+        if( pthread_create( &sniffer_thread , NULL ,  connection_handler , (void*) new_sock) < 0) {
             perror("could not create thread");
             return 1;
         }
-         
-        //Now join the thread , so that we dont terminate before the thread
-        //pthread_join( sniffer_thread , NULL);
+        
         puts("Handler assigned");  
-	  
     }
-     
-    if (client_sock < 0)
-    {
+    
+    if (client_sock < 0) {
         perror("accept failed");
         return 1;
     }
      
     return 0;
 }
- 
-/*
- * This will handle connection for each client
- * */
-void *connection_handler(void *socket_desc)
-{
-    //Get the socket descriptor
+
+
+// Implementasi pthread sehingga program tidak perlu
+// menunggu satu request selesai dahulu untuk memproses
+// request lain dari client lain.
+void *connection_handler(void *socket_desc) {
+    // Untuk socket programming
     int sock = *(int*)socket_desc;
+
+    // Nantinya variabel ini akan berisi ukuran dari
+    // request yang diterima dari pengguna
     int read_size;
-    char *message , client_message[2000];
-    char verify[]="Pesan diterima";
+
+    // Deklarasi variabel yang digunakan untuk menyimpan
+    // request dari pengguna.
+    char *message, client_message[2000];
+
+    // Balasan dari request yang dilakukan oleh pengguna
+    char verify[] = "Pesan diterima";
+
+    // Ini adalah perintah yang nantinya akan dikirim
+    // ke database MySQL, database akan merespon dari
+    // apapun yang kita kirimkan kepadanya.
+    // Query adalah cara program ini berkomunikasi dengan
+    // database MySQL.
     char *query = malloc(64);
   
+    // Apabila ada request dari pengguna
+    while( (read_size = recv(sock, client_message , 2000 , 0)) > 0 ) {
+    	// Menginisiasi sambungan MySQL
+    	MYSQL *con = mysql_init(NULL);
+      
+        // Program akan berhenti di sini jika terdapat
+        // eror saat menginisaisi MySQL.
+      	if (con == NULL) {
+    	      fprintf(stderr, "%s\n", mysql_error(con));
+    	      exit(1);
+    	}  
 
-	
+        // Melakukan koneksi dengan database MySQL
+        if (mysql_real_connect(con, "localhost", "admin", "adminpass", "alpro", 0, NULL, 0) == NULL) {
+            finish_with_error(con);
+        }   
+    	
+        // Mengisi variabel 'query' dengan query yang akan dikirimkan ke MySQL.
+        // Maksud dari query ini ialah untuk membuat baris baru pada tabel 'msg',
+        // yang mana pada kolom 'Pesan' akan dimasukan pesan dari pengguna.
+    	sprintf(query,"INSERT INTO `msg`(`Pesan`) VALUES(\"%s\")", client_message);
 
+        // Mengirimkan query tersebut ke MySQL
+    	if (mysql_query(con, query)) {
+    	      finish_with_error(con);
+    	} 
+    	
+        // Saat program sudah selesai mengirim query ke MySQL yang mana
+        // adalah untuk menyimpan pesan pengguna di database, maka program
+        // menghapus isi dari 'client_message'.
+    	memset(client_message, 0, sizeof client_message);
 
-    //Receive a message from client
-    while( (read_size = recv(sock , client_message , 2000 , 0)) > 0 )
-    {
-	
-	
-	//////// Start of MySQL ///////
-	MYSQL *con = mysql_init(NULL);
-  
-  	if (con == NULL) {
-	      fprintf(stderr, "%s\n", mysql_error(con));
-	      exit(1);
-	}  
+        // Menutup koneksi dengan MySQL karena sudah selesai
+        // urusannya dengan database.
+    	mysql_close(con);    
 
-	  if (mysql_real_connect(con, "localhost", "admin", "adminpass", 
-		  "alpro", 0, NULL, 0) == NULL) 
-	  {
-	      finish_with_error(con);
-	  }   
-	
-	sprintf(query,"INSERT INTO `msg`(`Pesan`) VALUES(\"%s\")",client_message);
-	if (mysql_query(con, query)) {
-	      finish_with_error(con);
-	  } 
-		
-	memset(client_message, 0, sizeof client_message);
-	  mysql_close(con);    
-
-	//Send the message back to client
-        write(sock , verify , strlen(verify));
-	
+    	// Mengirimkan respon ke pengguna bahwa request sudah diterima
+        // dan diproses.
+        write(sock, verify, strlen(verify));
     }
 
-    if(read_size == 0)
-    {
+    // Memberi kabar apabila koneksi kepada pengguna
+    // sudah selesai
+    if(read_size == 0) {
         puts("Client disconnected");
         fflush(stdout);
     }
-    else if(read_size == -1)
-    {
+
+    else if(read_size == -1) {
         perror("recv failed");
     }
          
-    //Free the socket pointer
+    // Membebaskan alokasi memori untuk
+    // socket programming
     free(socket_desc);
     
+    // Menutup koneksi soket
 	close(sock);
     return 0;
 }
 
-void finish_with_error(MYSQL *con)
-{
+
+// Fungsi ini akan dijalankan apabila 
+// pada proses yang berkaitan dengan MySQL
+// terdapat error.
+void finish_with_error(MYSQL *con) {
   fprintf(stderr, "%s\n", mysql_error(con));
   mysql_close(con);
   exit(1);        
--- serverRx.c
+++ serverRx.c
@@ -1,8 +1,12 @@
+// Program ini dijalankan di server, yang mana
+// dalam hal ini kita menggunakan Ubuntu Server 16.04 LTS
+// di alamat 103.43.44.105 (http://chatmote.com)
+
+// Melampirkan library-library yang dibutuhkan untuk
+// socket programming.
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-
-// Untuk server
 #include<sys/socket.h>
 #include<arpa/inet.h>
 #include<unistd.h>
@@ -15,60 +19,70 @@
 #include<pthread.h>
 void *connection_handler(void *);
 
-// int msgCount = 0;
-// int *msgCountPtr = &msgCount;
-
+// Fungsi ini akan dijalankan apabila 
+// pada proses yang berkaitan dengan MySQL
+// terdapat error.
 void finish_with_error(MYSQL *con) {
     fprintf(stderr, "%s\n", mysql_error(con));
     mysql_close(con);
     exit(1);        
 }
 
+// Ini adalah fungsi utama yang tugasnya adalah untuk
+// mengirimkan pesan dari database MySQL ke pengguna.
+// Pesan mana yang diambil dari database MySQL dan
+// dikirim ke pengguna itu tergantung dari nomor (ID)
+// pesan yang diminta
 int main(int argc, char *argv[]) {
+    // Untuk keperluan socket programming
     int socket_desc, client_sock, c, *new_sock;
     struct sockaddr_in server, client;
      
-    //Create socket
+    // Membuat socket untuk komunikasi dengan pengguna
     socket_desc = socket(AF_INET , SOCK_STREAM , 0);
     if (socket_desc == -1) {
         printf("Could not create socket");
-    }
-    puts("Socket created");
+    } puts("Socket created");
      
-    //Prepare the sockaddr_in structure
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY;
+
+    // Program ini akan mendengarkan (listen) port
+    // 8080.
     server.sin_port = htons(8080);
      
-    //Bind
+    // Membuat program mendengarkan (listen) request yang disampaikan oleh
+    // pengguna, dan mengabarkan apakah hal tersebut dilakukan dengan sukses
     if (bind(socket_desc, (struct sockaddr *)&server, sizeof(server)) < 0) {
-        //print the error message
         perror("bind failed. Error");
         return 1;
-    }
-    puts("bind done");
-     
-    //Listen
+    } puts("bind done");
     listen(socket_desc , 3);
      
-    //Accept and incoming connection
+    // Mengabarkan bahwa program ini sudah siap dan sedang 
+    // menunggu request dari komputer pengguna
     puts("Waiting for incoming connections...");
     c = sizeof(struct sockaddr_in);
 
+    // Bagian ini akan dijalankan jika program menerima request dari
+    // komputer pengguna.
     while( (client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) ) {
         puts("Connection accepted");
-         
+        
+        // Untuk pthread
         pthread_t sniffer_thread;
+
+        // Untuk socket programming
         new_sock = malloc(1);
         *new_sock = client_sock;
-         
+        
+        // Implementasi pthread sehingga request yang diterima selanjutnya akan
+        // akan diproses pada thread lain (thread baru).
         if( pthread_create(&sniffer_thread, NULL,  connection_handler, (void*) new_sock) < 0) {
             perror("could not create thread");
             return 1;
         }
-         
-        //Now join the thread , so that we dont terminate before the thread
-        //pthread_join( sniffer_thread , NULL);
+
         puts("Handler assigned");
     }
      
@@ -80,18 +94,30 @@ int main(int argc, char *argv[]) {
     return 0;
 }
  
-/*
- * This will handle connection for each client
- * */
+
+// Implementasi pthread sehingga program tidak perlu
+// menunggu satu request selesai dahulu untuk memproses
+// request lain dari client lain.
 void *connection_handler(void *socket_desc) {
-    //Get the socket descriptor
+    // Untuk socket programming
     int sock = *(int*)socket_desc;
+
+    // Nantinya variabel ini akan berisi ukuran dari
+    // request yang diterima dari pengguna
     int recv_size;
-    int request;
+
+    // Deklarasi variabel yang digunakan untuk menyimpan
+    // request dari pengguna.
     char client_message[16];
+
+    // Ini adalah perintah yang nantinya akan dikirim
+    // ke database MySQL, database akan merespon dari
+    // apapun yang kita kirimkan kepadanya.
+    // Query adalah cara program ini berkomunikasi dengan
+    // database MySQL.
     char *query = malloc(64);
 
-    //Receive a message from client
+    // Apabila ada request dari pengguna
     while( (recv_size = recv(sock, client_message, 16, 0)) > 0 ) {
         // Koneksi dengan database MYSQL
         MYSQL *con = mysql_init(NULL);
@@ -99,40 +125,63 @@ void *connection_handler(void *socket_desc) {
             finish_with_error(con);
         }
 
+        // Mengisi variabel 'query' dengan query yang akan dikirimkan ke MySQL.
+        // Maksud dari query ini ialah untuk mengambil (fetch) kolom 'Pesan' 
+        // pada tabel 'msg', dimana ID dari pesan tersebut harus sama dengan
+        // ID yang diminta oleh pengguna melalui request tadi.
         sprintf(query, "SELECT `Pesan` FROM msg WHERE `Id` = %d;", atoi(client_message));
 
+        // Mengeksekusi query tersebut
         if (mysql_query(con, query))  {
             finish_with_error(con);
         }
 
+        // Ini adalah variabel yang menyimpan respon dari MySQL
         MYSQL_RES *result = mysql_store_result(con);
 
+        // Apabila MySQL tidak memberikan respon apa-apa, 
+        // tampilkan error.
         if (result == NULL) {
             finish_with_error(con);
         }
 
+        // Untuk menyimpan pesan yang tadi diambil dari database
         char *message = malloc(100);
+
+        // Mengisi variabel ini dengan jumlah baris
+        // yang dikirimkan oleh database MySQL.
+        // Pada program ini, jumlah baris yang diterima 
+        // pasti sama dengan 1 karena program ini hanya 
+        // meminta satu pesan dengan ID tertentu saja.
         int num_fields = mysql_num_fields(result);
         MYSQL_ROW row;
 
+        // Pesan yang diambil dari database akan
+        // diletakkan di variabel 'message'
         int i;
         while ((row = mysql_fetch_row(result))) { 
-            for(i = 0; i < num_fields; i++) { 
-                //printf("%s", row[i] ? row[i] : "NULL");
+            for(i = 0; i < num_fields; i++) {
                 sprintf(message, "%s", row[i]);
-		//if (strcmp(message, NULL) != 0) printf("helo");
             } 
             printf("\n"); 
         }
 
+        // Membebaskan alokasi memori yang tadi untuk menyimpan
+        // hasil pengambilan pesan dari database, dan menutup
+        // koneksi dengan database
         mysql_free_result(result);
         mysql_close(con);
 
+        // Mengirimkan kembali pesan yang diambil dari database
+        // sebagai respon atas request yang dikirim pengguna.
         write(sock, message, strlen(message));
-	//write(sock, client_message, strlen(client_message));
-	memset(client_message, 0, sizeof(client_message));
+
+        // Program menghapus isi dari 'client_message'.
+	    memset(client_message, 0, sizeof(client_message));
     }
 
+    // Memberi kabar apabila koneksi kepada pengguna
+    // sudah selesai
     if(recv_size == 0) {
         puts("Client disconnected");
         fflush(stdout);
@@ -142,9 +191,11 @@ void *connection_handler(void *socket_desc) {
         perror("recv failed");
     }
          
-    //Free the socket pointer
+    // Membebaskan alokasi memori untuk
+    // socket programming
     free(socket_desc);
     
+    // Menutup koneksi soket
 	close(sock);
     return 0;
 }
